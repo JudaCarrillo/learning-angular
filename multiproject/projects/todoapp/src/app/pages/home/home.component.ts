e8bd69eff6d0921bf1d2397eefa60a71f01fa368
Juda Carrillo <130939221+JudaCarrillo@users.noreply.github.com>
2024-03-03 19:41:51 -0500

    upd: build

--- multiproject/projects/todoapp/src/app/pages/home/home.component.ts
+++ multiproject/projects/todoapp/src/app/pages/home/home.component.ts
@@ -1,12 +1,119 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  OnInit,
+  computed,
+  effect,
+  signal,
+  inject,
+  Injector,
+} from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Task } from '../../models/task.models';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  tasks = signal<Task[]>([]);
 
+  filter = signal<'all' | 'pending' | 'completed'>('all');
+  taskByFilter = computed(() => {
+    const filter = this.filter();
+    const tasks = this.tasks();
+
+    if (filter === 'pending') {
+      return tasks.filter((task) => !task.completed);
+    }
+    if (filter === 'completed') {
+      return tasks.filter((task) => task.completed);
+    }
+    return tasks;
+  });
+
+  newTaskCtrl = new FormControl('', {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern('^\\S.*$'),
+    ],
+  });
+
+  injector = inject(Injector);
+  ngOnInit(): void {
+    const storage = localStorage.getItem('tasks');
+    if (storage) {
+      this.tasks.set(JSON.parse(storage));
+    }
+
+    this.trackTasks();
+  }
+
+  changeHandler() {
+    if (this.newTaskCtrl.valid) {
+      const value = this.newTaskCtrl.value;
+      this.addTask(value);
+      this.newTaskCtrl.setValue('');
+    }
+  }
+
+  trackTasks() {
+    effect(
+      () => {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks()));
+      },
+      { injector: this.injector }
+    );
+  }
+
+  addTask(title: string) {
+    const newTask = {
+      id: Date.now(),
+      title,
+      completed: false,
+    };
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) => tasks.filter((task, i) => i !== index));
+  }
+
+  updateTask(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, i) =>
+        i === index ? { ...task, completed: !task.completed } : task
+      );
+    });
+  }
+
+  updateTaskEditingMode(index: number) {
+    this.tasks.update((tasks) => {
+      return tasks.map((task, i) =>
+        i === index && !task.completed
+          ? { ...task, editing: !task.editing }
+          : { ...task, editing: false }
+      );
+    });
+  }
+
+  updateTaskText(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+
+    this.tasks.update((tasks) => {
+      return tasks.map((task, i) =>
+        i === index && !task.completed
+          ? { ...task, title: input.value, editing: false }
+          : { ...task, editing: false }
+      );
+    });
+  }
+
+  changeFilter(filter: 'all' | 'pending' | 'completed') {
+    this.filter.set(filter);
+  }
 }
--- multiproject/projects/todoapp/src/app/pages/labs/labs.component.ts
+++ multiproject/projects/todoapp/src/app/pages/labs/labs.component.ts
@@ -1,47 +1,105 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
-export class LabsComponent {
+export class LabsComponent implements OnInit, OnDestroy {
   welcome = 'Bienvenido a mi primera aplicación con Angular';
-  tasks = [
+  tasks = signal([
     'Instalar el Angular CLI',
     'Crear proyecto',
-    'Crear componentes'
-  ];
+    'Crear componentes',
+    'Crear servicio',
+  ]);
   name = signal('Judá');
   disabled = true;
-  img = 'https://judacarrillo.github.io/PersonalPortfolio/assets/img/favicon.ico';
-  person = {
+  img =
+    'https://judacarrillo.github.io/PersonalPortfolio/assets/img/favicon.ico';
+  person = signal({
     name: 'Judá',
     age: 18,
-    avatar : 'https://judacarrillo.github.io/PersonalPortfolio/assets/img/favicon.ico'
-  };
-  
+    avatar:
+      'https://judacarrillo.github.io/PersonalPortfolio/assets/img/favicon.ico',
+  });
+
   private age = 18;
 
-  getAge () {
-    return this.age
+  colorCtrl = new FormControl();
+  widthCtrl = new FormControl(50, {
+    nonNullable: true,
+  });
+  nameCtrl = new FormControl(50, {
+    nonNullable: true,
+    validators: [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.pattern('^\\S.*$'),
+    ],
+  });
+
+  sub: Subscription | null = null;
+
+  ngOnInit(): void {
+    this.handleFormSubscription();
   }
 
-  clickHandler () {
-    alert('Hola')
+  ngOnDestroy(): void {
+    if (this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 
-  changeHandler (e: Event) {
+  getAge() {
+    return this.age;
+  }
+
+  handleFormSubscription() {
+    this.sub = this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+
+  clickHandler() {
+    alert('Hola');
+  }
+
+  changeHandler(e: Event) {
     const input = e.target as HTMLInputElement;
     const newValue = input.value;
     this.name.set(newValue);
   }
 
-  keydownHandler (e: KeyboardEvent) {
-    const input = e.target as HTMLInputElement
-    console.log(input.value);    
+  changeAge(e: Event) {
+    const input = e.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update((prevState) => {
+      return {
+        ...prevState,
+        age: parseInt(newValue, 10),
+      };
+    });
+  }
+
+  changeName(e: Event) {
+    const input = e.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update((prevState) => {
+      return {
+        ...prevState,
+        name: newValue,
+      };
+    });
+  }
+
+  keydownHandler(e: KeyboardEvent) {
+    const input = e.target as HTMLInputElement;
+    console.log(input.value);
   }
 }
