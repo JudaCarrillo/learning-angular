262a938510c098522caf29cecec2d95dde09f519
Juda Carrillo <130939221+JudaCarrillo@users.noreply.github.com>
2024-03-25 20:38:16 -0500

    feat: deploy in vercel

--- multiproject/tailwind.config.js
+++ multiproject/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{html,ts}",
+        "./projects/store/src/**/*.{html,ts}",
     ],
     theme: {
         extend: {},
