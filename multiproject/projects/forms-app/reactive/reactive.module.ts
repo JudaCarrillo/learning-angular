42c7baa80cde6158870fe80128aa120ed4d45163
Juda <130939221+JudaCarrillo@users.noreply.github.com>
2024-05-30 21:32:55 -0500

    feat: create basic components

--- multiproject/projects/forms-app/reactive/reactive.module.ts
+++ multiproject/projects/forms-app/reactive/reactive.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
+import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 import { ReactiveRoutingModule } from './reactive-routing.module';
 
-
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+  declarations: [
+    BasicPageComponent,
+    DynamicPageComponent,
+    SwitchesPageComponent,
+  ],
+  imports: [CommonModule, ReactiveRoutingModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
